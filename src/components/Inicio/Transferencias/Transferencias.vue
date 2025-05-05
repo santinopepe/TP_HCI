37ae6d87f39b72b0b0bdc404c9c9fbe90cf96631
maraujofeltrin <manuel.araujofeltrin@gmail.com>
2025-05-05 10:31:36 -0300

    correccion de botones

--- src/components/Inicio/Transferencias/Transferencias.vue
+++ src/components/Inicio/Transferencias/Transferencias.vue
@@ -46,7 +46,7 @@
         <div class="max-w-md mx-auto mt-6 text-center">
           <button
             @click="mostrarFormularioNuevoContacto"
-            class="bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-600"
+            class="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700"
           >
             Transferir a un nuevo contacto
           </button>
--- src/components/Inicio/Transferencias/TransferenciasNuevoContacto.vue
+++ src/components/Inicio/Transferencias/TransferenciasNuevoContacto.vue
@@ -44,7 +44,7 @@
           </button>
           <button
             type="submit"
-            class="bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-600"
+            class="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700"
           >
             Guardar
           </button>
