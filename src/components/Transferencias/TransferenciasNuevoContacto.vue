591e41da5ca9281ec81e150ae671d6ff07b16873
MartinGallardoB <mgallardobertuzzi@itba.edu.ar>
2025-05-19 21:25:33 -0300

    Cambio del color de fondo de la pantalla de agregar contacto
    Co-Authored-By: Manuel Araujo Feltrin <152537869+maraujofeltrin@users.noreply.github.com>
    Co-Authored-By: santinopepe <146956051+santinopepe@users.noreply.github.com>
    Co-Authored-By: Santiago Nogueira <147627792+snogueira1@users.noreply.github.com>

--- src/components/Transferencias/TransferenciasNuevoContacto.vue
+++ src/components/Transferencias/TransferenciasNuevoContacto.vue
@@ -1,41 +1,43 @@
 <template>
-  <div class="items-center justify-center max-w-md mx-auto mt-36 bg-white p-6 rounded-lg shadow">  
-    <BotonRetroceder />
-    <h2 class="text-2xl font-bold text-gray-800 mb-4 text-center">Nuevo Contacto</h2>
-    <form @submit.prevent="guardarContacto">
-      <div class="mb-4">
-        <label for="nombre" class="block text-gray-700 font-medium mb-2">Nombre</label>
-        <input
-          v-model="nuevoContacto.nombre"
-          type="text"
-          id="nombre"
-          placeholder="Nombre del contacto"
-          class="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring focus:ring-blue-300"
-          :class="{ 'border-red-500': errores.nombre }"
-        />
-        <p v-if="errores.nombre" class="text-red-500 text-sm mt-1">{{ errores.nombre }}</p>
-      </div>
-      <div class="mb-4">
-        <label for="cbu" class="block text-gray-700 font-medium mb-2">CBU/CVU</label>
-        <input
-          v-model="nuevoContacto.cbu"
-          type="text"
-          id="cbu"
-          placeholder="CBU/CVU del contacto"
-          class="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring focus:ring-blue-300"
-          :class="{ 'border-red-500': errores.cbu }"
-        />
-        <p v-if="errores.cbu" class="text-red-500 text-sm mt-1">{{ errores.cbu }}</p>
-      </div>
-      <div class="flex justify-center items-center">
-        <button
-          type="submit"
-          class="bg-[#5D8C39] text-white px-12 py-2 rounded-lg hover:bg-[#5D8C39]/80"
-        >
-          Guardar
-        </button>
-      </div>
-    </form>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div class="max-w-md w-full bg-white p-6 rounded-lg shadow flex flex-col items-center">
+      <BotonRetroceder />
+      <h2 class="text-2xl font-bold text-gray-800 mb-4 text-center">Nuevo Contacto</h2>
+      <form @submit.prevent="guardarContacto" class="w-full">
+        <div class="mb-4">
+          <label for="nombre" class="block text-gray-700 font-medium mb-2">Nombre</label>
+          <input
+            v-model="nuevoContacto.nombre"
+            type="text"
+            id="nombre"
+            placeholder="Nombre del contacto"
+            class="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring focus:ring-blue-300"
+            :class="{ 'border-red-500': errores.nombre }"
+          />
+          <p v-if="errores.nombre" class="text-red-500 text-sm mt-1">{{ errores.nombre }}</p>
+        </div>
+        <div class="mb-4">
+          <label for="cbu" class="block text-gray-700 font-medium mb-2">CBU/CVU</label>
+          <input
+            v-model="nuevoContacto.cbu"
+            type="text"
+            id="cbu"
+            placeholder="CBU/CVU del contacto"
+            class="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring focus:ring-blue-300"
+            :class="{ 'border-red-500': errores.cbu }"
+          />
+          <p v-if="errores.cbu" class="text-red-500 text-sm mt-1">{{ errores.cbu }}</p>
+        </div>
+        <div class="flex justify-center items-center">
+          <button
+            type="submit"
+            class="bg-[#5D8C39] text-white px-12 py-2 rounded-lg hover:bg-[#5D8C39]/80"
+          >
+            Guardar
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
